92862755ec533b567cc2cc1073a6a0e6434cfbf0
Guillermo Gutierrez <guillegr123@gmail.com>
2019-01-28 11:07:57 -0600

    0504 - Pagination - Displaying Pages

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import _ from "lodash"; // Optimized version of Underscore.js
 
 const Pagination = props => {
-  return null;
+  const { itemsCount, pageSize } = props;
+
+  const pagesCount = Math.ceil(itemsCount / pageSize);
+  if (pagesCount === 1) return null; // Nothing will be rendered
+  const pages = _.range(1, pagesCount + 1);
+
+  return (
+    <nav>
+      <ul class="pagination">
+        {pages.map(page => (
+          <li key={page} class="page-item">
+            <a class="page-link">{page}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Pagination;
