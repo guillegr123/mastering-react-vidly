d9fd46447a74947702f13df0b4aa66d5461844f3
Guillermo Gutierrez <guillegr123@gmail.com>
2019-02-20 15:25:38 -0600

    0827 - Exercise - Connect Movie Form to the Backend

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -7,6 +7,21 @@ export function getMovies() {
   return http.get(apiEndpoint);
 }
 
+export function getMovie(movieId) {
+  return http.get(`${apiEndpoint}/${movieId}`);
+}
+
+export function saveMovie(movie) {
+  const movieId = movie._id;
+
+  if (movieId) {
+    delete movie._id;
+    return http.put(`${apiEndpoint}/${movieId}`, movie);
+  } else {
+    return http.post(apiEndpoint, movie);
+  }
+}
+
 export function deleteMovie(movieId) {
   return http.delete(apiEndpoint + "/" + movieId);
 }
