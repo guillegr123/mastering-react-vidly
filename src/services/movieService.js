11b206650bfb34a254a7b8ff9b154bf7e70e8445
Guillermo Gutierrez <guillegr123@gmail.com>
2019-02-20 15:49:44 -0600

    0831 - Refactoring

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,12 +3,16 @@ import { apiUrl } from "../config.json";
 
 const apiEndpoint = apiUrl + "/movies";
 
+function movieUrl(id) {
+  return `${apiEndpoint}/${id}`;
+}
+
 export function getMovies() {
   return http.get(apiEndpoint);
 }
 
 export function getMovie(movieId) {
-  return http.get(apiEndpoint + "/" + movieId);
+  return http.get(movieUrl(movieId));
 }
 
 export function saveMovie(movie) {
@@ -17,12 +21,12 @@ export function saveMovie(movie) {
     // because `movie` is stored in the MovieForm component state (see movieForm.jsx, method
     // `doSubmit`).
     delete body._id;
-    return http.put(apiEndpoint + "/" + movie._id, body);
+    return http.put(movieUrl(movie._id), body);
   }
 
   return http.post(apiEndpoint, movie);
 }
 
 export function deleteMovie(movieId) {
-  return http.delete(apiEndpoint + "/" + movieId);
+  return http.delete(movieUrl(movieId));
 }
