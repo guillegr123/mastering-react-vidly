d660134f37cbc2dfa2ff3d527b69a251ed819ede
Guillermo Gutierrez <guillegr123@gmail.com>
2019-02-25 14:25:25 -0600

    0920 - Extracting ProtectedRoute

---
+++ src/components/common/protectedRoute.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import auth from "../../services/authService";
+
+const ProtectedRoute = ({ component: Component, render, ...rest }) => {
+  return (
+    <Route
+      {...rest} // To pass any additional properties
+      render={props => {
+        if (!auth.getCurrentUser()) return <Redirect to="/login" />;
+        return Component ? <Component {...props} /> : render(props); // Component should start with capital letter, so the transpiler for React does not complains
+      }}
+    />
+  );
+};
+
+export default ProtectedRoute;
