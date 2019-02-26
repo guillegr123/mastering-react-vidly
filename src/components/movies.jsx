a161dc85f1e81c145134b8413a1869a03f32f0cc
Guillermo Gutierrez <guillegr123@gmail.com>
2019-02-26 10:00:10 -0600

    1010 - Preparing the Front-end for Deployment

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -116,8 +116,6 @@ class Movies extends Component {
     const { length: count } = allMovies;
     const { user } = this.props;
 
-    if (count === 0) return <p>There are no movies in the database.</p>;
-
     const { totalCount, data: movies } = this.getPagedData();
 
     // ListGroup, MoviesTable and Pagination are high level components, that have similar level
